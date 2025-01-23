bde29a40088c60090f8a786162135d34c2f5db1c
rw <rw@20sec.net>
2025-01-23 12:48:39 +0100

    start buildscript for raspi

---
+++ build-raspi.sh
@@ -0,0 +1,16 @@
+CGO_ENABLED=1 \
+    XCADDY_SETCAP=1 \
+    XCADDY_GO_BUILD_FLAGS="-ldflags='-w -s' -tags=nobadger,nomysql,nopgx" \
+    CGO_CFLAGS=$(php-config --includes) \
+    CGO_LDFLAGS="$(php-config --ldflags) $(php-config --libs)" \
+    xcaddy build \
+        --output /usr/local/bin/frankenphp \
+        --with github.com/dunglas/frankenphp=./ \
+        --with github.com/dunglas/frankenphp/caddy=./caddy/ \
+        --with github.com/dunglas/caddy-cbrotli \
+        # Mercure and Vulcain are included in the official build, but feel free to remove them
+        --with github.com/dunglas/mercure/caddy \
+        # --with github.com/dunglas/vulcain/caddy
+        # Add extra Caddy modules here
+		--with github.com/quix-labs/caddy-image-processor \
+		--with github.com/baldinof/caddy-supervisor
