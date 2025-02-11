2dd84267764f88dfd770a9b4deacbce1a8019238
rw <rw@20sec.net>
2025-02-12 00:00:56 +0100

    install on raspi

--- script/install-raspi.sh
+++ script/install-raspi.sh
@@ -4,3 +4,7 @@ curl -O -L https://github.com/cwmoss/moopl-server/releases/download/v20250211/fr
 unzip franken.zip
 sudo cp release/local/bin/frankenphp /usr/local/bin/
 sudo cp -r release/local/lib/* /usr/local/lib/
+
+sudo setcap cap_net_bind_service=+ep $(which frankenphp)
+
+# frankenphp run --config raspi/Caddyfile --envfile .pubkey.env
